ac6fa32bc40c79551cc3c75a922a4027d97e10d0
Iqbal <iamhasan9501@gmail.com>
2023-03-17 01:20:42 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import useCountDown from "./hook/useCountdown";
 
 function App() {
-  const { days, hours, minutes, seconds } = useCountDown("15 March, 2024");
+  const { days, hours, minutes, seconds } = useCountDown("15 March, 2025");
 
   return (
     <div className="App">
@@ -11,6 +11,7 @@ function App() {
           {days}:{hours}:{minutes}:{seconds}
         </p>
       </header>
+      <p>End date: 15 March, 2025</p>
     </div>
   );
 }
